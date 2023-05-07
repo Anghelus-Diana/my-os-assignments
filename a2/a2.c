802b4d0bcd2f60b6e05b2828dc16b3db68fca77a
Anghelus-Diana <anghelus_diana@yahoo.com>
2023-05-07 17:52:30 +0300

    A 4 a incer are

--- a2/a2.c
+++ a2/a2.c
@@ -15,29 +15,38 @@ typedef struct
     sem_t logSem[2];
 } TH_STRUCT;
 
+// semafoare pentru Procesul 7
 sem_t semafor1;
 sem_t semafor2;
 
+// semafoare pentru Procesele 8 si 7
+sem_t *semafor3 = NULL;
+sem_t *semafor4 = NULL;
+
+//semfor pentru cele 5 threduri simulane 
+
+sem_t semafor5;
+
 
 void *thread_function7(void *arg)
-{   
+{
     TH_STRUCT *s = (TH_STRUCT *)arg;
     // int id = *(int*)arg;
 
-     //Thread 3
-    //semafor1=(sem_t)(s->logSem[0]);
-    //semafor2=(sem_t)(s->logSem[1]);
+    // Thread 3
+    // semafor1=(sem_t)(s->logSem[0]);
+    // semafor2=(sem_t)(s->logSem[1]);
 
-     if (s->value == 3)
+    if (s->value == 3)
     {
-        
+
         info(BEGIN, 7, s->value);
         sem_post(&semafor1);
 
         sem_wait(&semafor2);
         info(END, 7, s->value);
     }
-     
+
     else if (s->value == 1)
     {
         sem_wait(&semafor1);
@@ -47,15 +56,7 @@ void *thread_function7(void *arg)
         sem_post(&semafor2);
     }
 
-
-    
-   
-     //Thread 1
-    
-
-    
-    
-     //Thread 2
+    // Thread 2
     if (s->value == 2)
     {
         info(BEGIN, 7, s->value);
@@ -65,16 +66,15 @@ void *thread_function7(void *arg)
     {
         info(END, 7, s->value);
     }
-    
-     //Thread 4
+
+    // Thread 4
     if (s->value == 4)
     {
+        sem_wait(semafor3);
         info(BEGIN, 7, s->value);
-    }
-    
-    if (s->value == 4)
-    {
+
         info(END, 7, s->value);
+        sem_post(semafor4);
     }
     return NULL;
 }
@@ -84,14 +84,33 @@ void *thread_function5(void *arg)
     int id = *(int *)arg;
     info(BEGIN, 5, id);
     info(END, 5, id);
+    sem_post(&semafor5);
     return NULL;
 }
 
 void *thread_function8(void *arg)
 {
     int id = *(int *)arg;
-    info(BEGIN, 8, id);
-    info(END, 8, id);
+
+    if (id == 1)
+    {
+        info(BEGIN, 8, id);
+        sem_post(semafor3);
+
+        info(END, 8, id);
+    }
+    else if (id == 4)
+    {
+        sem_wait(semafor4);
+        info(BEGIN, 8, id);
+        info(END, 8, id);
+    }
+    else
+    {
+        info(BEGIN, 8, id);
+        info(END, 8, id);
+    }
+
     return NULL;
 }
 
@@ -100,6 +119,18 @@ int main(int argc, char **argv)
     init();
 
     info(BEGIN, 1, 0);
+    semafor3 = sem_open("/l08p1_log_semaphore", O_CREAT, 0644, 0);
+    semafor4 = sem_open("/l08p1_log_semaphore2", O_CREAT, 0644, 0);
+    if (semafor3 == NULL)
+    {
+        perror("Could not aquire the semaphore");
+        return -1;
+    }
+    if (semafor4 == NULL)
+    {
+        perror("Could not aquire the semaphore");
+        return -1;
+    }
     pid_t pid2 = fork();
 
     // PROCESUL 2
@@ -118,10 +149,15 @@ int main(int argc, char **argv)
             printf("eroare");
             return -1;
         }
-
+   
         else if (pid5 == 0)
         {
             info(BEGIN, 5, 0);
+            if (sem_init(&semafor5, 0, 5) != 0)
+                {
+                    perror("Could not init the semaphore");
+                    return -1;
+                }
             // Creare threaduri in procesul 5
             pthread_t tid[45];
 
@@ -129,13 +165,15 @@ int main(int argc, char **argv)
             int ids[45] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45};
             for (i = 0; i < 45; i++)
             {
-
+                sem_wait(&semafor5);
                 pthread_create(&tid[i], NULL, thread_function5, &ids[i]);
             }
             for (i = 0; i < 45; i++)
             {
                 pthread_join(tid[i], NULL);
             }
+             
+            sem_destroy(&semafor5);
 
             // PROCESUL 6
             pid_t pid6 = fork();
@@ -163,12 +201,12 @@ int main(int argc, char **argv)
                 // Creare threaduri in procesul 7
                 pthread_t tid[4];
                 TH_STRUCT params[4];
-                //sem_t logSem[2];
+                // sem_t logSem[2];
 
                 int i = 0;
-                //int ids[4] = {1, 2, 3, 4};
-                //sem_destroy(&logSem[0]);
-                //sem_destroy(&logSem[1]);
+                // int ids[4] = {1, 2, 3, 4};
+                // sem_destroy(&logSem[0]);
+                // sem_destroy(&logSem[1]);
                 if (sem_init(&semafor1, 0, 0) != 0)
                 {
                     perror("Could not init the semaphore");
@@ -179,11 +217,12 @@ int main(int argc, char **argv)
                     perror("Could not init the semaphore");
                     return -1;
                 }
+
                 for (i = 0; i < 4; i++)
                 {
                     params[i].value = i + 1;
-                    //params[i].logSem[0] = logSem[0];
-                    //params[i].logSem[1] = logSem[1];
+                    // params[i].logSem[0] = logSem[0];
+                    // params[i].logSem[1] = logSem[1];
 
                     pthread_create(&tid[i], NULL, thread_function7, &params[i]);
                 }
@@ -286,6 +325,7 @@ int main(int argc, char **argv)
     waitpid(pid3, NULL, 0);
     info(END, 1, 0);
     exit(0);
-
+    sem_close(semafor3);
+    sem_close(semafor4);
     return 0;
 }
